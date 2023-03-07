d0023263c768651c42e88bd1548dd09cf7310f75
Augusto Neves <augustoneves@frwk.com.br>
2023-03-07 11:07:38 -0300

    Creating random button component

---
+++ src/components/RandomButton/index.tsx
@@ -0,0 +1,52 @@
+import RandomIcon from "../../assets/random.svg";
+import { View, TouchableOpacity, Text } from "react-native";
+import { searchPokemon } from "../../service/axios";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+
+export function RandomButton() {
+  const navigation = useNavigation();
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  async function handleRandomSearch() {
+    setIsDisabled(true);
+    const minValue = 1;
+    const maxValue = 1008;
+
+    let randomValue = (
+      Math.random() * (maxValue - minValue) +
+      minValue
+    ).toFixed();
+
+    try {
+      const pokemonResult = (await searchPokemon(randomValue)) as any;
+
+      navigation.navigate("Pokemon", {
+        pokemon: {
+          ...pokemonResult,
+          sprite:
+            pokemonResult.sprites?.other["official-artwork"].front_default,
+        },
+      });
+
+      setIsDisabled(false);
+    } catch (error) {
+      console.log(error);
+      setIsDisabled(false);
+    }
+  }
+
+  return (
+    <View className="absolute right-4 bottom-4 z-40">
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className="flex-row items-center justify-evenly h-16 w-44 rounded-2xl bg-yellow-400 p-5 pb-5"
+        onPress={handleRandomSearch}
+        disabled={isDisabled}
+      >
+        <RandomIcon height={40} width={40} />
+        <Text className="font-medium text-base">Random</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
