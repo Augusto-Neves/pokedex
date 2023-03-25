0aaad9e8c922c424bf53e34e558cb4a46fac91fc
Augusto Neves <augustoneves@frwk.com.br>
2023-03-24 21:32:06 -0300

    Adding react-native-portalize components

--- App.tsx
+++ App.tsx
@@ -9,10 +9,15 @@ import {
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from "react";
 import { LogBox } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 SplashScreen.preventAutoHideAsync();
 
-LogBox.ignoreLogs(["Request failed with status code 404"]);
+LogBox.ignoreLogs([
+  "Request failed with status code 404",
+  "Warning: This synthetic event is reused for performance reasons.",
+  "Possible Unhandled Promise Rejection",
+]);
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,9 +37,9 @@ export default function App() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView className="flex-1">
       <Routes onReady={onLayoutRootView} />
       <StatusBar style="dark" />
-    </>
+    </GestureHandlerRootView>
   );
 }
--- src/routes/HomeTabScreen/index.tsx
+++ src/routes/HomeTabScreen/index.tsx
@@ -4,69 +4,72 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ComparatorScreen } from "../../screens/ComparatorScreen";
 import { FavoriteScreen } from "../../screens/FavoritesScreen";
 import { QuizScreen } from "../../screens/QuizScreen";
+import { Host } from "react-native-portalize";
 
 const Tab = createBottomTabNavigator();
 
 export function HomeTabScreen() {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, size, color }) => {
-          let iconName: any;
+    <Host>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, size, color }) => {
+            let iconName: any;
 
-          switch (route.name) {
-            case "HomeTab":
-              iconName = focused ? "home" : "home";
-              break;
-            case "Comparator":
-              iconName = focused ? "compare-arrows" : "compare-arrows";
-              break;
-            case "Quiz":
-              iconName = focused ? "question-answer" : "question-answer";
-              break;
-            case "Favorite":
-              iconName = focused ? "favorite" : "favorite";
-              break;
-            default:
-              iconName = "";
-          }
+            switch (route.name) {
+              case "HomeTab":
+                iconName = focused ? "home" : "home";
+                break;
+              case "Comparator":
+                iconName = focused ? "compare-arrows" : "compare-arrows";
+                break;
+              case "Quiz":
+                iconName = focused ? "question-answer" : "question-answer";
+                break;
+              case "Favorite":
+                iconName = focused ? "favorite" : "favorite";
+                break;
+              default:
+                iconName = "";
+            }
 
-          return <MaterialIcons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: "#283141",
-        tabBarInactiveTintColor: "#8D9DB9",
-        headerShown: false,
-        tabBarLabelStyle: {
-          paddingBottom: 10,
-          fontSize: 10,
-          fontWeight: "600",
-        },
-        tabBarStyle: {
-          padding: 10,
-          height: 70,
-        },
-      })}
-    >
-      <Tab.Screen
-        name="HomeTab"
-        component={HomeScreen}
-        options={{ title: "Home" }}
-      />
-      <Tab.Screen
-        name="Comparator"
-        component={ComparatorScreen}
-        options={{ title: "Comparator" }}
-      />
-      <Tab.Screen
-        name="Quiz"
-        component={QuizScreen}
-        options={{ title: "Quiz" }}
-      />
-      <Tab.Screen
-        name="Favorite"
-        component={FavoriteScreen}
-        options={{ title: "Favorites" }}
-      />
-    </Tab.Navigator>
+            return <MaterialIcons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#283141",
+          tabBarInactiveTintColor: "#8D9DB9",
+          headerShown: false,
+          tabBarLabelStyle: {
+            paddingBottom: 10,
+            fontSize: 10,
+            fontWeight: "600",
+          },
+          tabBarStyle: {
+            padding: 10,
+            height: 70,
+          },
+        })}
+      >
+        <Tab.Screen
+          name="HomeTab"
+          component={HomeScreen}
+          options={{ title: "Home" }}
+        />
+        <Tab.Screen
+          name="Comparator"
+          component={ComparatorScreen}
+          options={{ title: "Comparator" }}
+        />
+        <Tab.Screen
+          name="Quiz"
+          component={QuizScreen}
+          options={{ title: "Quiz" }}
+        />
+        <Tab.Screen
+          name="Favorite"
+          component={FavoriteScreen}
+          options={{ title: "Favorites" }}
+        />
+      </Tab.Navigator>
+    </Host>
   );
 }
