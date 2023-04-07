a98e39c34c449fff3c8bacaa8b31a09e655f2125
Augusto Neves <augustoneves@frwk.com.br>
2023-04-07 13:08:43 -0300

    filtering moves by unique name.

--- src/screens/PokemonMovesScreen.tsx
+++ src/screens/PokemonMovesScreen.tsx
@@ -1,20 +1,21 @@
 import { useRoute } from "@react-navigation/native";
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, FlatList } from "react-native";
 import { ParamsProps } from "../@types/pokemonParamsProps";
 import { MoveRow } from "../components/MoveRow";
 
 export function PokemonMovesScreen() {
   const route = useRoute();
   const pokemon = route.params as ParamsProps;
-  const orderedMoves = [...pokemon.moves].sort((a, b) =>
-    a.move.name.localeCompare(b.move.name)
-  );
+  const orderedMoves = [...pokemon.moves]
+    .sort((a, b) => a.move.name.localeCompare(b.move.name))
+    .map((moves) => moves.move.name);
+  const uniqueMoves = [...new Set(orderedMoves)];
 
   return (
     <ScrollView className="px-10 flex-1" nestedScrollEnabled>
       <View className="pb-28">
-        {orderedMoves.map((data) => (
-          <MoveRow move={data.move.name} key={data.move.name} />
+        {uniqueMoves.map((move) => (
+          <MoveRow move={move} key={move} />
         ))}
       </View>
     </ScrollView>
