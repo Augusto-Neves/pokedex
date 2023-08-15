213965fd26b32537577d5e6ba8f1790c03407ea0
Augusto Neves <augustoneves@frwk.com.br>
2023-08-14 22:00:08 -0300

    refactor: using function returnPokemonTypeIcon

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -9,63 +9,15 @@ import {
 import { capitalizeFirstLetter } from "../../utils/capitalizeFirstLetter";
 import { addingPadToString } from "../../utils/addingPadToString";
 import { ParamsProps } from "../../@types/pokemonParamsProps";
-
-// Pokemons Types Icons
-import GrassIcon from "../../assets/typeIcons/grass.svg";
-import FireIcon from "../../assets/typeIcons/fire.svg";
-import WaterIcon from "../../assets/typeIcons/water.svg";
-import BugIcon from "../../assets/typeIcons/bug.svg";
-import NormalIcon from "../../assets/typeIcons/normal.svg";
-import PoisonIcon from "../../assets/typeIcons/poison.svg";
-import ElectricIcon from "../../assets/typeIcons/electric.svg";
-import GroundIcon from "../../assets/typeIcons/ground.svg";
-import RockIcon from "../../assets/typeIcons/rock.svg";
-import PsychicIcon from "../../assets/typeIcons/psychic.svg";
-import FightingIcon from "../../assets/typeIcons/fighting.svg";
-import GhostIcon from "../../assets/typeIcons/ghost.svg";
-import FlyingIcon from "../../assets/typeIcons/flying.svg";
-import FairyIcon from "../../assets/typeIcons/fairy.svg";
-import IceIcon from "../../assets/typeIcons/ice.svg";
-import DragonIcon from "../../assets/typeIcons/dragon.svg";
-import DarkIcon from "../../assets/typeIcons/dark.svg";
-import SteelIcon from "../../assets/typeIcons/steel.svg";
+import { returnPokemonTypeIcon } from "../../utils/returnPokemonTypeIcon";
 
 interface PokemonCardProps {
   pokemon: ParamsProps | any;
   onPress?: (pokemon: any) => void | (() => void);
 }
 
-export function PokemonCard(
-  this: any,
-  { pokemon, onPress, ...rest }: PokemonCardProps
-) {
+export function PokemonCard(this: any, { pokemon, onPress, ...rest }: PokemonCardProps) {
   const pokemonFirstType = pokemon?.types[0].type.name;
-
-  function returnPokemonTypeIcon(pokemonType: string) {
-    const pokemonTypes = {
-      grass: <GrassIcon height={15} width={15} />,
-      fire: <FireIcon height={15} width={15} />,
-      water: <WaterIcon height={15} width={15} />,
-      bug: <BugIcon height={15} width={15} />,
-      normal: <NormalIcon height={15} width={15} />,
-      poison: <PoisonIcon height={15} width={15} />,
-      electric: <ElectricIcon height={15} width={15} />,
-      ground: <GroundIcon height={15} width={15} />,
-      rock: <RockIcon height={15} width={15} />,
-      psychic: <PsychicIcon height={15} width={15} />,
-      fighting: <FightingIcon height={15} width={15} />,
-      ghost: <GhostIcon height={15} width={15} />,
-      flying: <FlyingIcon height={15} width={15} />,
-      fairy: <FairyIcon height={15} width={15} />,
-      ice: <IceIcon height={15} width={15} />,
-      dragon: <DragonIcon height={15} width={15} />,
-      dark: <DarkIcon height={15} width={15} />,
-      steel: <SteelIcon height={15} width={15} />,
-      default: "",
-    };
-
-    return (pokemonTypes as any)[pokemonType] || pokemonTypes.default;
-  }
 
   return (
     <TouchableOpacity
@@ -73,26 +25,26 @@ export function PokemonCard(
       activeOpacity={0.7}
       onPress={onPress?.bind(this, pokemon)}
       className={clsx(
-        "w-[170px] h-[115px] items-center justify-center rounded-lg space-x-2  mx-3 my-3",
+        'w-[170px] h-[115px] items-center justify-center rounded-lg space-x-2  mx-3 my-3',
         {
-          "bg-pokemon-type-grass": pokemonFirstType === "grass",
-          "bg-pokemon-type-fire": pokemonFirstType === "fire",
-          "bg-pokemon-type-water": pokemonFirstType === "water",
-          "bg-pokemon-type-bug": pokemonFirstType === "bug",
-          "bg-pokemon-type-normal": pokemonFirstType === "normal",
-          "bg-pokemon-type-poison": pokemonFirstType === "poison",
-          "bg-pokemon-type-electric": pokemonFirstType === "electric",
-          "bg-pokemon-type-ground": pokemonFirstType === "ground",
-          "bg-pokemon-type-rock": pokemonFirstType === "rock",
-          "bg-pokemon-type-psychic": pokemonFirstType === "psychic",
-          "bg-pokemon-type-fighting": pokemonFirstType === "fighting",
-          "bg-pokemon-type-ghost": pokemonFirstType === "ghost",
-          "bg-pokemon-type-flying": pokemonFirstType === "flying",
-          "bg-pokemon-type-fairy": pokemonFirstType === "fairy",
-          "bg-pokemon-type-ice": pokemonFirstType === "ice",
-          "bg-pokemon-type-dragon": pokemonFirstType === "dragon",
-          "bg-pokemon-type-dark": pokemonFirstType === "dark",
-          "bg-pokemon-type-steel": pokemonFirstType === "steel",
+          'bg-pokemon-type-grass': pokemonFirstType === 'grass',
+          'bg-pokemon-type-fire': pokemonFirstType === 'fire',
+          'bg-pokemon-type-water': pokemonFirstType === 'water',
+          'bg-pokemon-type-bug': pokemonFirstType === 'bug',
+          'bg-pokemon-type-normal': pokemonFirstType === 'normal',
+          'bg-pokemon-type-poison': pokemonFirstType === 'poison',
+          'bg-pokemon-type-electric': pokemonFirstType === 'electric',
+          'bg-pokemon-type-ground': pokemonFirstType === 'ground',
+          'bg-pokemon-type-rock': pokemonFirstType === 'rock',
+          'bg-pokemon-type-psychic': pokemonFirstType === 'psychic',
+          'bg-pokemon-type-fighting': pokemonFirstType === 'fighting',
+          'bg-pokemon-type-ghost': pokemonFirstType === 'ghost',
+          'bg-pokemon-type-flying': pokemonFirstType === 'flying',
+          'bg-pokemon-type-fairy': pokemonFirstType === 'fairy',
+          'bg-pokemon-type-ice': pokemonFirstType === 'ice',
+          'bg-pokemon-type-dragon': pokemonFirstType === 'dragon',
+          'bg-pokemon-type-dark': pokemonFirstType === 'dark',
+          'bg-pokemon-type-steel': pokemonFirstType === 'steel',
         }
       )}
     >
@@ -124,7 +76,7 @@ export function PokemonCard(
           ))}
         </View>
         <ImageBackground
-          source={require("../../assets/pokeball.png")}
+          source={require('../../assets/pokeball.png')}
           resizeMode="contain"
           className="items-center justify-center w-20 h-20"
         >
